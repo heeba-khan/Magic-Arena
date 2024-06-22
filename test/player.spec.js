64e0ed5dc652ae2b353adfd9374788c5b4fba670
heeba-khan <heebak45@gmail.com>
2024-06-23 00:04:34 +0530

    Implemented player.spec.js to test Player class

---
+++ test/player.spec.js
@@ -0,0 +1,31 @@
+
+const Player = require('../src/models/player');
+
+describe('Player Class', () => {
+    let player;
+
+    beforeEach(() => {
+        player = new Player('Hero1', 100, 50, 20);
+    });
+
+    test('should create a player with the given properties', () => {
+        expect(player.name).toBe('Hero1');
+        expect(player.health).toBe(100);
+        expect(player.strength).toBe(50);
+        expect(player.attack).toBe(20);
+    });
+
+    test('should return true if the player is alive', () => {
+        expect(player.isAlive()).toBe(true);
+    });
+
+    test('should return false if the player is not alive', () => {
+        player.health = 0;
+        expect(player.isAlive()).toBe(false);
+    });
+
+    test('should return false if the player health is negative', () => {
+        player.health = -10;
+        expect(player.isAlive()).toBe(false);
+    });
+});
