df6420c404cb61029bb26d06e13ba9e007b1d047
heeba-khan <heebak45@gmail.com>
2024-06-22 23:52:36 +0530

    Normalized all line endings

---
+++ src/models/player.js
@@ -0,0 +1,14 @@
+class Player{
+    constructor(name,health,strength,attack){
+        this.name=name
+        this.health=health
+        this.strength=strength
+        this.attack=attack
+    }
+
+    isAlive(){
+        return this.health>0
+    }
+}
+
+module.exports=Player
